1a7e6998ad7f0bb9aff4bcbef69a3a1e64ca1234
sam253narula <samarthnarula253@gmail.com>
2021-09-22 22:43:57 +0530

    Adding use of different Thread pools

--- ExecutorService/src/com/example/executorservice/fixedthreadpool/ExecutorDemo.java
+++ ExecutorService/src/com/example/executorservice/fixedthreadpool/ExecutorDemo.java
@@ -8,6 +8,15 @@ public class ExecutorDemo {
 	public static void main(String[] args) {
 		// Create a fixed thread pool with maximum 3 threads
 		ExecutorService executor = Executors.newFixedThreadPool(2);
+		
+		//Creates Threads as needed and deletes the inactive threads after 60 seconds of inactivity
+		//ExecutorService executor = Executors.newCachedThreadPool();
+		
+		//Executes only one thread
+	//	ExecutorService executor = Executors.newSingleThreadExecutor(); 
+		 
+		//Internally manages thread pool of 10 threads to run scheduled tasks
+	//	ExecutorService executor = Executors.newScheduledThreadPool(10); 
 		// Submit runnable tasks to the executor
 		executor.execute(new PrintChar('a', 100));
 		executor.execute(new PrintChar('b', 100));
