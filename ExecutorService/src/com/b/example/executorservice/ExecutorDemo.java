b3e2216dcbabcf889804996e745c9119751e1324
sam253narula <samarthnarula253@gmail.com>
2021-09-23 08:44:00 +0530

    Adding ExecutorService Scheduled Task Example

--- ExecutorService/src/com/b/example/executorservice/ExecutorDemo.java
+++ ExecutorService/src/com/b/example/executorservice/ExecutorDemo.java
@@ -1,11 +1,16 @@
 package com.b.example.executorservice;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 public class ExecutorDemo {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
 		// Create a fixed thread pool with maximum 3 threads
 		ExecutorService executor = Executors.newFixedThreadPool(2);
 		
@@ -17,6 +22,24 @@ public class ExecutorDemo {
 		 
 		//Internally manages thread pool of 10 threads to run scheduled tasks
 	//	ExecutorService executor = Executors.newScheduledThreadPool(10); 
+//		ScheduledExecutorService executor =
+//		        Executors.newScheduledThreadPool(5);
+//
+//		ScheduledFuture scheduledFuture =
+//				executor.schedule(new Callable() {
+//		        public Object call() throws Exception {
+//		        	System.out.println();
+//		            System.out.println("Executed!");
+//		            return "Called!";
+//		        }
+//		    },
+//		    5,
+//		    TimeUnit.SECONDS);
+//		
+//		while(scheduledFuture.isDone()) {
+//			System.out.println(scheduledFuture.get());
+//		}
+		
 		// Submit runnable tasks to the executor
 		executor.execute(new PrintChar('a', 100));
 		executor.execute(new PrintChar('b', 100));
--- MultiThreading/src/com/h/deadlock/problem/DeadLockDemo.java
+++ MultiThreading/src/com/h/deadlock/problem/DeadLockDemo.java
@@ -3,7 +3,7 @@ package com.h.deadlock.problem;
 
 public class DeadLockDemo {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		final Class1 obj1 = new Class1();
 		final Class2 obj2 = new Class2();
 		
